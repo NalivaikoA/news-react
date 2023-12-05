ef0fe8cb4734cc1114ef270b59e1a6bc7350ec69
NalivaikoA <nalivayko09@rambler.ru>
2023-12-05 17:42:53 +0300

    migration from js to ts

--- src/components/NewsBanner/NewsBanner.tsx
+++ src/components/NewsBanner/NewsBanner.tsx
@@ -1,8 +1,13 @@
 import { formatTimeAgo } from "../../helpers/formatTimeAgo";
+import { INews } from "../../interfaces";
 import { Image } from "../Image/Image";
 import styles from "./styles.module.css";
 
-export const NewsBanner = ({ item }) => {
+interface Props {
+  item: INews;
+}
+
+export const NewsBanner = ({ item }: Props) => {
   return (
     <div className={styles.banner}>
       <Image image={item?.image} />
